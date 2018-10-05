03efd4c27292099a766711326020eec5b08f99a6
suliman <sulimantwfiq@gmail.com>
2018-10-05 03:58:14 +0300

    gggg

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ export default class IndexPage extends React.Component {
               <div className="content" style={{ border: "1px solid #eaecee", padding: "2em 4em" }} key={post.id}>
                 <p>
                   <Link className="has-text-primary" to={post.fields.slug}>
-                    {post.frontmatter.title}
+                    {post.frontmatter.title} ggggggggg
                   </Link>
                   <span> &bull; </span>
                   <small>{post.frontmatter.date}</small>
