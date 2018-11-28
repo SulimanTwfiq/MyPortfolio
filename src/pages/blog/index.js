c4730df8e046f5dfccd1d9fa1ae681e07e6b9879
Suliman <sulimantwfiq@gmail.com>
2018-11-29 00:19:21 +0300

    add css grid to blog

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -4,23 +4,38 @@ import styled from "styled-components";
 import Layout from "../../components/Layout";
 import { graphql } from "gatsby";
 import { DefaultCard } from "../../components/Card";
-import Button from "../../components/Button";
-
+import media from "../../components/media";
 const StyledPost = styled.div`
   border-radius: 10px;
   background-color: rgba(255, 255, 255, 0.5);
   padding: 10px;
   margin: 7px 0;
   border: 2px rgba(0, 0, 0, 0.2) dotted;
-
-  small {
-    font-size: 0.6rem;
+  p {
+    font-size: 0.8rem;
   }
-  img{
-    width:150px;
-    display:block;
-    margin:0 auto;
+  img {
+    width: 150px;
+    display: block;
+    margin: 0 auto;
   }
+  ${media.tablet`
+  display:grid;  
+  grid-template-areas: 
+  "img title"
+  "img desc";
+   img{
+    grid-area: img;
+  height:100%;
+  }
+   p{
+    grid-area: desc
+  }
+   h4{
+    grid-area: title
+  }
+  
+  `}
 `;
 const Blog = () => {
   return (
@@ -57,18 +72,14 @@ const Blog = () => {
             <DefaultCard center>
               {posts.map(({ node: post }) => (
                 <StyledPost key={post.id}>
-                {console.log(post.excerpt)}
-                  <h3>
+                  <h4>
                     <Link to={post.fields.slug}>{post.frontmatter.title}</Link>
-                    {/* <small>{post.frontmatter.date}</small> */}
-                  </h3>
-                  <img src={post.frontmatter.thumbnail} alt={post.frontmatter.title}/>
-                  <p>
-                    {post.excerpt}
-                  </p>
-                    <Button>
-                      <Link to={post.fields.slug}>تابع القراءة </Link>
-                    </Button>
+                  </h4>
+                  <img
+                    src={post.frontmatter.thumbnail}
+                    alt={post.frontmatter.title}
+                  />
+                  <p>{post.excerpt}</p>
                 </StyledPost>
               ))}
             </DefaultCard>
